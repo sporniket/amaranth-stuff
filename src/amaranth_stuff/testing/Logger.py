488374334fa859b21e43a1dca1157e934f9647c6
David SPORN <sporniket.studio@gmail.com>
2023-08-30 07:04:22 +0200

    Resolves #6 : [testing] a class to log a signal

---
+++ src/amaranth_stuff/testing/Logger.py
@@ -0,0 +1,47 @@
+"""
+---
+(c) 2022,2023 David SPORN
+---
+This is part of Sporniket's "Amaranth Stuff" project.
+
+Sporniket's "Amaranth Stuff" project is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+Sporniket's "Amaranth Stuff" project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along with Sporniket's "Amaranth Stuff" project.
+If not, see <https://www.gnu.org/licenses/>. 
+---
+"""
+
+### builtin deps
+from typing import List  # , Dict, Tuple, Optional
+
+### amaranth -- main deps
+from amaranth import *
+from amaranth.build import Platform
+
+
+class Logger(Elaboratable):
+    def __init__(self, source, size):
+        self.source = source
+        self.logs = [
+            Signal(source.shape(), reset=source.reset) for i in range(0, size + 1)
+        ]
+
+    def ports(self):
+        return self.logs
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.d.sync += self.logs[0].eq(self.source)
+        for i in range(1, len(self.logs)):
+            m.d.sync += self.logs[i].eq(self.logs[i - 1])
+
+        return m
---
+++ tests/testing/test_Logger.py
@@ -0,0 +1,83 @@
+"""
+---
+(c) 2022,2023 David SPORN
+---
+This is part of Sporniket's "Amaranth Stuff" project.
+
+Sporniket's "Amaranth Stuff" project is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+Sporniket's "Amaranth Stuff" project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along with Sporniket's "Amaranth Stuff" project.
+If not, see <https://www.gnu.org/licenses/>. 
+---
+"""
+import pytest
+from subprocess import CalledProcessError
+
+### amaranth -- main deps
+from amaranth import *
+from amaranth.hdl.ir import UnusedElaboratable
+
+### amaranth -- test deps
+from amaranth.asserts import *  # AnyConst, AnySeq, Assert, Assume, Cover, Past, Stable, Rose, Fell, Initial
+
+### amarant-stuff deps
+from amaranth_stuff.testing.Logger import *
+from amaranth_stuff.testing import Test
+
+
+###
+### Test suite on Logger
+###
+
+
+def test_Logger_should_use_signals_with_same_shape_and_reset_value():
+    logger = Logger(Signal(signed(5), reset=3), 6)
+    assert logger.source.shape().width == 5 and logger.source.shape().signed == True
+    assert len(logger.logs) == 7
+    for i in logger.logs:
+        assert i.shape().width == 5 and i.shape().signed == True and i.reset == 3
+
+
+def test_Logger_should_log_signal_history():
+    def testBody(m: Module, cd: ClockDomain):
+        rst = cd.rst
+        tb = m.submodules.testBench
+        logger = m.submodules.dut
+
+        din = tb.registerPort(
+            logger.source
+        )  # otherwise cannot manipulate the source of the logger.
+        witness = [
+            tb.registerPort(Signal()) for i in range(0, 6)
+        ]  # 6 signals from 0 to 5
+        m.d.sync += [witness[0].eq(din)] + [
+            witness[i].eq(witness[i - 1]) for i in range(1, 6)
+        ]
+
+        # When witness exhibits the values [0,0,1,0,1,1]
+        with m.If(
+            ~witness[0]
+            & ~witness[1]
+            & witness[2]
+            & ~witness[3]
+            & witness[4]
+            & witness[5]
+        ):
+            m.d.sync += Assert(
+                ~logger.logs[0]
+                & ~logger.logs[1]
+                & logger.logs[2]
+                & ~logger.logs[3]
+                & logger.logs[4]
+                & logger.logs[5]
+            )
+
+    Test.describe(Logger(Signal(), 5), testBody)
