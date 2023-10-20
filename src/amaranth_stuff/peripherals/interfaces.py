befd252015ab63a1f212c126139e6907f1d2683f
David SPORN <sporniket.studio@gmail.com>
2023-10-20 22:25:03 +0200

    Resolves #53 : [peripherals] Model of Signature for the data bus

---
+++ src/amaranth_stuff/peripherals/interfaces.py
@@ -0,0 +1,40 @@
+"""
+---
+(c) 2022,2023 David SPORN
+---
+This is part of Sporniket's "Amaranth Stuff" project.
+
+Sporniket's "Amaranth Stuff" project is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+Sporniket's "Amaranth Stuff" project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along with Sporniket's "Amaranth Stuff" project.
+If not, see <https://www.gnu.org/licenses/>. 
+---
+"""
+### builtin deps
+from typing import List  # , Dict, Tuple, Optional
+
+### amaranth -- main deps
+from amaranth import *
+from amaranth.lib.wiring import *
+
+
+class SignatureOfDataBus(Signature):
+    """
+    The data bus is the channel that allow a CPU and peripherals to exchange data. It has a given width.
+    """
+
+    def __init__(self, width: int):
+        super.__init__(
+            {
+                "toCentral": In(width),
+                "toPeripheral": Out(width),
+            }
+        )
